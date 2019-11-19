c1e505683a20ad958f962241af90eb6a8e6d097f
Alin M <alinpaulmacovei@gmail.com>
2019-11-19 12:41:13 +0000

    npm run build

---
+++ public/precache-manifest.dd31237c9254d4b280f0c7980b1d8cf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e1614d68117734bfab796e5ef854c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4596f249d745e2c18dd",
+    "url": "/static/css/main.bda34dc3.chunk.css"
+  },
+  {
+    "revision": "b95c2f16e3af3f2d2b05",
+    "url": "/static/js/2.0beebcc8.chunk.js"
+  },
+  {
+    "revision": "d4596f249d745e2c18dd",
+    "url": "/static/js/main.118ae851.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
