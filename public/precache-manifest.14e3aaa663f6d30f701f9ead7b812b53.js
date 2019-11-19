35f53401cdb899ae77672fb60a5d35475bc0ad52
Alin M <alinpaulmacovei@gmail.com>
2019-11-19 21:39:13 +0000

    tmp

---
+++ public/precache-manifest.14e3aaa663f6d30f701f9ead7b812b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4843b306f75012f810fac2045209cf78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ee2043c071b291898e",
+    "url": "/static/css/main.444458fc.chunk.css"
+  },
+  {
+    "revision": "668caf6ea0796851bee3",
+    "url": "/static/js/2.bad69f21.chunk.js"
+  },
+  {
+    "revision": "88ee2043c071b291898e",
+    "url": "/static/js/main.b2840568.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.681354896d17eecc8c2b233b3fcd2d56.js"
+  "/precache-manifest.14e3aaa663f6d30f701f9ead7b812b53.js"
 );
 
 self.addEventListener('message', (event) => {
